02af20d487fab48ffc0ddbe487ffd6ef4537628c
Srivani <srivani.indurupalli@gmail.com>
2021-07-20 14:39:31 +0400

    added break demo

---
+++ Project2/src/package2/BreakDemo.java
@@ -0,0 +1,25 @@
+package package2;
+
+public class BreakDemo
+{
+
+	public static void main(String[] args)
+	{
+		int num=0;
+		//outerBrk is name of label , if we break label the outer will break
+	   outerBrk:for(int i=0;i<10;i++)
+	   {
+		   for(int j=0;j<10;j++)
+		   {
+			   if(i==5 && j==5)
+			   {
+				   break outerBrk;
+				   
+			   }
+			   num++;
+		   }
+	   }
+	   System.out.println("Num:"+num);
+	}
+
+}
